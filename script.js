302fb7ea436d943bf0f68e8a48392129441e11b5
simsim2006 <simsim2006@hotmail.com>
2022-08-19 19:32:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
       
       document.querySelector('button').addEventListener('click', () => {
         console.log('clicked');
-        navigator.registerProtocolHandler('web+cooiliuyt', './?burger=%s');
+        navigator.registerProtocolHandler('web+cooiliuyt', 'https://simsim2006.github.io/protocolhandler/?burger=%s');
       });
 })();
     
